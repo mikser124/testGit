f7b1f74c68d60a9a60e591388f69f5d527a05b5b
Nazar <s97817@pollub.edu.pl>
2024-10-10 14:00:05 +0200

    git init

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hi");
+        System.out.println("hi 21412");
+
+
     }
 }
